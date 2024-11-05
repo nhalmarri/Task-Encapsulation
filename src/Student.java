b74000556dddc1d67a4179b08a0619e4924b3eec
Nora <nhalmarri64@gmail.com>
2024-11-05 09:44:11 +0300

    Task done

---
+++ src/Student.java
@@ -0,0 +1,64 @@
+public class Student {
+
+    private String name;
+    private int age;
+    private double gpa;
+
+    //constructor
+    public Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    //getters
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGpaStatus() {
+
+        String status ;
+
+        if( gpa > 3){
+            status = "Excellent!";
+            return status;
+        } else if ( gpa <= 3 && gpa > 2){
+            status = "Good.";
+            return status;
+        } else {
+            status = "Needs Improvement.";
+            return status;
+        }
+
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGpa(double gpa) {
+
+        if ( gpa < 0.0 || gpa > 4.0){
+            System.out.println("GPA out of range !!");
+        }
+
+    }
+
+
+
+
+
+
+
+} //end of class
